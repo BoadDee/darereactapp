0bb0168b58881e17c276b6f489c7a2e1378740ba
boadeakintunde <boadeakintunde@gmail.com>
2024-03-17 17:53:57 -0600

    dsddd

--- webAppcode/jest.config.js
+++
@@ -1,1 +0,0 @@
-setupFilesAfterEnv: ['<rootDir>/setupTests.js']
--- webAppcode/src/App.test.js
+++ webAppcode/src/App.test.js
@@ -1,11 +1,9 @@
 
-import "@testing-library/jest-dom/extend-expect";
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
-
-// test('renders app title element', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/Dare React testing/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+test('renders app title element', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Dare React testing/i);
+  expect(linkElement).toBeInTheDocument();
+});
